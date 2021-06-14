53bb8f18919194a1d08b3e00d04f3f2580f4b33d
Jetal C. Mali <malijetal1234@gmail.com>
2021-06-15 04:01:48 +0530

    useReducer part-2:Todo

--- README.md
+++ README.md
@@ -46,3 +46,5 @@
 
 - useReducer() like wise useState() is used to manage state and rerender the component whnever state changes.
 - It set states to the new version of the state using type of action.
+- useReducer() passed `initial value` as an `object`
+- It has dispatch where we can pass type and payload and reducer function can call another function to perform `fetch, payload define` etc. 
--- src/UseReducer.js
+++ src/UseReducer.js
@@ -1,30 +1,65 @@
-import React, { useReducer } from "react";
+import React, { useState, useReducer } from "react";
+
+import Todo from "./Todo";
+
+const ACTIONS = {
+  INCREMENT: "increment",
+  DECREMENT: "decrement",
+  ADD_TODO: "add-Todo",
+  TOGGLE_TODO: "toggle-Todo",
+};
 
 function reducer(state, action) {
   switch (action.type) {
-    case "increment":
+    case ACTIONS.INCREMENT:
       return { count: state.count + 1 };
-    case "decrement":
+    case ACTIONS.DECREMENT:
       return { count: state.count - 1 };
+    case ACTIONS.ADD_TODO:
+      return [...state, newTodo(action.payload.name)];
+    case ACTIONS.TOGGLE_TODO:
+      break;
+    default:
+      return state;
   }
 }
 
+function newTodo(name) {
+  return { id: Date.now(), name: name, complete: false };
+}
+
 export default function UseReducer() {
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  //const [state, dispatch] = useReducer(reducer, { count: 0 });
+  // function incrementCount() {
+  //   dispatch({ type: "increment" });
+  // }
 
-  function incrementCount() {
-    dispatch({ type: "increment" });
+  // function decrementCount() {
+  //   dispatch({ type: "decrement" });
+  // }
+  const [todos, dispatch] = useReducer(reducer, []);
+  const [name, setName] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } });
+    setName("");
   }
 
-  function decrementCount() {
-    dispatch({ type: "decrement" });
-  }
+  console.log(todos);
 
   return (
     <div>
-      <button onClick={decrementCount}>-</button>
-      {state.count}
-      <button onClick={incrementCount}>+</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </form>
+      {todos.map((todo) => {
+        return <Todo key={todo.id} todo={todo} />;
+      })}
     </div>
   );
 }
